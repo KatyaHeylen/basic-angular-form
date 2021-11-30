e2d1782e1d9d574f3c40f35f7c3908b77927375a
Katya Heylen <ezhgurova@gmail.com>
2021-11-30 15:26:43 +0100

    validation added

---
+++ project-name/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Friend } from './friend';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'be my friend';
+  public friend = new Friend('', '', '','', '');
+
+}
---
+++ project-name/src/app/friend.ts
@@ -0,0 +1,21 @@
+export class Friend {
+  firstName: string;
+  lastName: string;
+  email: string;
+  number: string;
+  language: string;
+
+  constructor(
+  firstName: string,
+  lastName: string,
+  email: string,
+  number: string,
+  language: string,
+  ) {
+   this.firstName = firstName;
+   this.lastName = lastName;
+   this.email = email;
+   this.number = number;
+   this.language = language;
+  }
+}
